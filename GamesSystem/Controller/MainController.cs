2a3f05f9493725fa17c5b7028882a2917a9d15ea
stanko <stanko@STAN-LAPTOP>
2022-03-17 09:15:19 +0200

    Added the Create Method

--- GamesSystem/Controller/MainController.cs
+++ GamesSystem/Controller/MainController.cs
@@ -10,6 +10,15 @@ namespace GamesSystem.Controller
     class MainController
     {
         //Create - Team Leader
+        public void CreateGame(Game g)
+        {
+            using (GamesDBEntities gdbe = new GamesDBEntities())
+            {
+                g.Id = gdbe.Games.Count() + 1;
+                gdbe.Games.Add(g);
+                gdbe.SaveChanges();
+            }
+        }
 
         //Update - Dev 1
         //Delete - Dev 2
